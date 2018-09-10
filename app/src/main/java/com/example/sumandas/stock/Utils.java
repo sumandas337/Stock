f95b0c01ff64936d6fe787411129fdbd6b2ce5e4
Suman Das <suman.das@Sumans-MacBook-Pro.local>
2018-09-10 17:51:46 +0530

    fixed bug

--- app/src/main/java/com/example/sumandas/stock/Utils.java
+++ app/src/main/java/com/example/sumandas/stock/Utils.java
@@ -6,9 +6,9 @@ public class Utils {
         String[] t1 = time1.split(":");
         String[] t2 = time2.split(":");
         if(Integer.parseInt(t1[0]) == Integer.parseInt(t2[0])){
-            return Integer.parseInt(t1[0])- Integer.parseInt(t2[0])*-1;
+            return Integer.parseInt(t1[1])- Integer.parseInt(t2[1]);
         }else{
-            return Integer.parseInt(t1[0]) - Integer.parseInt(t2[0])*-1;
+            return Integer.parseInt(t1[0]) - Integer.parseInt(t2[0]);
         }
      }
 
